a3e343232ba8df78ec611b9d16a1170df0cee3f5
LuigiElleBalotta <luigibalotta@icloud.com>
2016-11-01 16:23:04 +0100

    Init

---
+++ RetailCharDump/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace RetailCharDump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SniffFileName = new System.Windows.Forms.TextBox();
+            this.CharInfoRTB = new System.Windows.Forms.RichTextBox();
+            this.ScanBTN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusLBL = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.SqlOutRTB = new System.Windows.Forms.RichTextBox();
+            this.GenSqlBTN = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CharGuidTXT = new System.Windows.Forms.TextBox();
+            this.tabControl2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sniff Name:";
+            // 
+            // SniffFileName
+            // 
+            this.SniffFileName.Location = new System.Drawing.Point(74, 9);
+            this.SniffFileName.Name = "SniffFileName";
+            this.SniffFileName.Size = new System.Drawing.Size(137, 20);
+            this.SniffFileName.TabIndex = 1;
+            // 
+            // CharInfoRTB
+            // 
+            this.CharInfoRTB.Location = new System.Drawing.Point(9, 79);
+            this.CharInfoRTB.Name = "CharInfoRTB";
+            this.CharInfoRTB.ReadOnly = true;
+            this.CharInfoRTB.Size = new System.Drawing.Size(256, 169);
+            this.CharInfoRTB.TabIndex = 2;
+            this.CharInfoRTB.Text = "";
+            // 
+            // ScanBTN
+            // 
+            this.ScanBTN.Location = new System.Drawing.Point(217, 7);
+            this.ScanBTN.Name = "ScanBTN";
+            this.ScanBTN.Size = new System.Drawing.Size(48, 23);
+            this.ScanBTN.TabIndex = 3;
+            this.ScanBTN.Text = "SCAN";
+            this.ScanBTN.UseVisualStyleBackColor = true;
+            this.ScanBTN.Click += new System.EventHandler(this.ScanBTN_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Status:";
+            // 
+            // statusLBL
+            // 
+            this.statusLBL.AutoSize = true;
+            this.statusLBL.Location = new System.Drawing.Point(52, 255);
+            this.statusLBL.Name = "statusLBL";
+            this.statusLBL.Size = new System.Drawing.Size(69, 13);
+            this.statusLBL.TabIndex = 5;
+            this.statusLBL.Text = "NO STATUS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sniff Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SCAN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ScanBTN_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "NO STATUS";
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Location = new System.Drawing.Point(12, 12);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(279, 304);
+            this.tabControl2.TabIndex = 6;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.CharGuidTXT);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.SniffFileName);
+            this.tabPage3.Controls.Add(this.statusLBL);
+            this.tabPage3.Controls.Add(this.CharInfoRTB);
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.ScanBTN);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(271, 278);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Input";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.GenSqlBTN);
+            this.tabPage4.Controls.Add(this.SqlOutRTB);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(271, 278);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "SQL";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // SqlOutRTB
+            // 
+            this.SqlOutRTB.Location = new System.Drawing.Point(7, 7);
+            this.SqlOutRTB.Name = "SqlOutRTB";
+            this.SqlOutRTB.Size = new System.Drawing.Size(258, 222);
+            this.SqlOutRTB.TabIndex = 0;
+            this.SqlOutRTB.Text = "";
+            // 
+            // GenSqlBTN
+            // 
+            this.GenSqlBTN.Location = new System.Drawing.Point(7, 236);
+            this.GenSqlBTN.Name = "GenSqlBTN";
+            this.GenSqlBTN.Size = new System.Drawing.Size(258, 36);
+            this.GenSqlBTN.TabIndex = 1;
+            this.GenSqlBTN.Text = "Create SQL File";
+            this.GenSqlBTN.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Character GUID:";
+            // 
+            // CharGuidTXT
+            // 
+            this.CharGuidTXT.Location = new System.Drawing.Point(101, 43);
+            this.CharGuidTXT.Name = "CharGuidTXT";
+            this.CharGuidTXT.Size = new System.Drawing.Size(43, 20);
+            this.CharGuidTXT.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 324);
+            this.Controls.Add(this.tabControl2);
+            this.Name = "Form1";
+            this.Text = "RetailCharDump";
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SniffFileName;
+        private System.Windows.Forms.RichTextBox CharInfoRTB;
+        private System.Windows.Forms.Button ScanBTN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label statusLBL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button GenSqlBTN;
+        private System.Windows.Forms.RichTextBox SqlOutRTB;
+        private System.Windows.Forms.TextBox CharGuidTXT;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ RetailCharDump/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace RetailCharDump
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetStringBetween(string STR, string FirstString, string LastString)
+        {
+            string FinalString;
+            int Pos1 = STR.IndexOf(FirstString) + FirstString.Length;
+            int Pos2 = STR.IndexOf(LastString);
+            if (LastString == Environment.NewLine)
+                FinalString = STR.Substring(Pos1, STR.Length - Pos1);
+            else
+                FinalString = STR.Substring(Pos1, Pos2 - Pos1);
+            return FinalString;
+        }
+
+        public long ConvertDateTimeToTimestamp(string date)
+        {
+            string[] str = date.Split(' ');
+            string[] ddmmaaaa = str[0].Split('/');
+            int month = Convert.ToInt16(ddmmaaaa[0]);
+            int day = Convert.ToInt16(ddmmaaaa[1]);
+            int year = Convert.ToInt16(ddmmaaaa[2]);
+
+            string[] hhmmss = str[1].Split(':');
+            int hour = Convert.ToInt16(hhmmss[0]);
+            int minute = Convert.ToInt16(hhmmss[1]);
+            int sec = Convert.ToInt16(hhmmss[2]);
+
+            DateTime datetime = new DateTime(year, month, day, hour, minute, sec);
+            var timeSpan = (datetime - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)timeSpan.TotalSeconds;
+        }
+
+        private void ScanBTN_Click(object sender, EventArgs e)
+        {
+            bool EnteredOpcode = false;
+            string FoundOpcode = String.Empty;
+
+            /*SEND KNOWN SPELLS VARIABLES (dbtable: character_spell)*/
+            bool SEND_KNOWN_SPELLS = false;
+            string KnownSpellsCount = String.Empty;
+            int ParsedKnownSpells = 0;
+            List<int> SpellIDList = new List<int>();
+
+            /*ALL ACHIEVEMENT DATA VARIABLES dbtable: character_achievement & character_achievement_progress*/
+            bool ALL_ACHIEVEMENT_DATA = false;
+            string EarnedCount = String.Empty;
+            string ProgressCount = String.Empty;
+            int ParsedAchiCount = 0;
+            int ParsedProgAchiCount = 0;
+            List<long> DateAchi = new List<long>();
+            List<int> AchiID = new List<int>();
+            List<int> AchiProgID = new List<int>();
+            List<int> QuantityAchiProg = new List<int>();
+            List<long> DateAchiProg = new List<long>();
+
+            /*UPDATE OBJECT VARIABLES*/
+            bool UPDATE_OBJECT = false;
+            string NumObjUpdates = String.Empty;
+            int ParsedObjUpdates = 0;
+            int DetectedItems = 0;
+            bool ObjUpdateItem = false;
+            List<int> ItemEntry = new List<int>();
+            List<int> ItemCount = new List<int>();
+            List<int> ItemFlags = new List<int>();
+            List<int> ItemDurability = new List<int>();
+
+            int counter = 0;
+            string line = String.Empty;
+            statusLBL.ResetText();
+            statusLBL.Text = "INITIAL SCANNING";
+            statusLBL.Refresh();
+            StreamReader file = new StreamReader($"{SniffFileName.Text}.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                statusLBL.Text = $"SCANNING LINE {counter++}";
+                statusLBL.Refresh();
+
+                /**************\
+                 * Elaboration*
+                \**************/
+                if (!EnteredOpcode)
+                {
+                    if (line.Contains("SMSG_SEND_KNOWN_SPELLS") && !SEND_KNOWN_SPELLS)
+                    {
+                        EnteredOpcode = true;
+                        SEND_KNOWN_SPELLS = true;
+                        FoundOpcode = "SMSG_SEND_KNOWN_SPELLS";
+                    }   
+
+                    if(line.Contains("SMSG_ALL_ACHIEVEMENT_DATA") && !ALL_ACHIEVEMENT_DATA)
+                    {
+                        EnteredOpcode = true;
+                        ALL_ACHIEVEMENT_DATA = true;
+                        FoundOpcode = "SMSG_ALL_ACHIEVEMENT_DATA";
+                    }
+
+                    if(line.Contains("SMSG_UPDATE_OBJECT") && !UPDATE_OBJECT) //We have player item only in the first strike of this opcode?
+                    {
+                        EnteredOpcode = true;
+                        UPDATE_OBJECT = true;
+                        FoundOpcode = "SMSG_UPDATE_OBJECT";
+                    }
+                }
+                else
+                {
+                    switch(FoundOpcode)
+                    {
+                        case "SMSG_SEND_KNOWN_SPELLS":
+                            if (KnownSpellsCount != "" && Convert.ToInt16(KnownSpellsCount) == ParsedKnownSpells)
+                                EnteredOpcode = false;
+
+                            if(line.Contains("KnownSpellsCount"))
+                                KnownSpellsCount = GetStringBetween(line, "KnownSpellsCount: ", " (");
+
+                            if(line.Contains("KnownSpellId"))
+                            {
+                                SpellIDList.Add(Convert.ToInt32(GetStringBetween(line, "KnownSpellId: ", " (")));
+                                ParsedKnownSpells++;
+                            }
+                            break;
+                        case "SMSG_ALL_ACHIEVEMENT_DATA":
+                            if ((EarnedCount != "" && Convert.ToInt32(EarnedCount) == ParsedAchiCount) && (ProgressCount != "" && Convert.ToInt32(ProgressCount) == ParsedProgAchiCount))
+                                EnteredOpcode = false;
+
+                            if (line.Contains("EarnedCount"))
+                                EarnedCount = GetStringBetween(line, "EarnedCount: ", " (");
+
+                            if (line.Contains("ProgressCount"))
+                                ProgressCount = GetStringBetween(line, "ProgressCount: ", " (");
+
+                            if (line.Contains("Earned") && line.Contains("Id"))
+                            {
+                                AchiID.Add(Convert.ToInt32(GetStringBetween(line, "Id: ", " (")));
+                                ParsedAchiCount++;
+                            }
+
+                            if (line.Contains("Earned") && line.Contains("Date"))
+                                DateAchi.Add(ConvertDateTimeToTimestamp(GetStringBetween(line, "Date: ", Environment.NewLine)));
+
+                            if (line.Contains("Progress") && line.Contains("Id"))
+                            {
+                                AchiProgID.Add(Convert.ToInt32(GetStringBetween(line, "Id: ", " (")));
+                                ParsedProgAchiCount++;
+                            }
+
+                            if (line.Contains("Progress") && line.Contains("Date"))
+                                DateAchiProg.Add(ConvertDateTimeToTimestamp(GetStringBetween(line, "Date: ", Environment.NewLine)));
+
+                            if (line.Contains("Progress") && line.Contains("Quantity"))
+                                QuantityAchiProg.Add(Convert.ToInt32(GetStringBetween(line, "Quantity: ", " (")));
+
+                            break;
+                        case "SMSG_UPDATE_OBJECT":
+                            if (NumObjUpdates != "" && Convert.ToInt16(NumObjUpdates) == ParsedObjUpdates)
+                                EnteredOpcode = false;
+
+                            if (line.Contains("NumObjUpdates"))
+                                NumObjUpdates = GetStringBetween(line, "NumObjUpdates: ", " (");
+
+                            if(line.Contains("Object Type"))
+                            {
+                                ParsedObjUpdates++;
+                                if (line.Contains("Item"))
+                                {
+                                    ObjUpdateItem = true;
+                                    DetectedItems++;
+                                }
+                                else
+                                    ObjUpdateItem = false;
+                            }
+                            if (ObjUpdateItem)
+                            {
+                                if (line.Contains("OBJECT_FIELD_ENTRY"))
+                                    ItemEntry.Add(Convert.ToInt32(GetStringBetween(line, "OBJECT_FIELD_ENTRY: ", "/")));
+
+                                if (line.Contains("ITEM_FIELD_FLAGS"))
+                                    ItemFlags.Add(Convert.ToInt32(GetStringBetween(line, "ITEM_FIELD_FLAGS: ", "/")));
+                                else
+                                    ItemFlags.Add(1);
+
+                                if (line.Contains("ITEM_FIELD_STACK_COUNT"))
+                                    ItemCount.Add(Convert.ToInt32(GetStringBetween(line, "ITEM_FIELD_STACK_COUNT: ", "/")));
+
+                                if (line.Contains("ITEM_FIELD_MAXDURABILITY"))
+                                    ItemDurability.Add(Convert.ToInt32(GetStringBetween(line, "ITEM_FIELD_MAXDURABILITY: ", "/")));
+                                else
+                                    ItemDurability.Add(0);
+
+                            }
+                            break;
+                    }
+                }
+
+                CharInfoRTB.AppendText($"{line} {Environment.NewLine}");
+                counter++;
+            }
+            file.Close();
+
+            statusLBL.ResetText();
+            statusLBL.Text = "CREATING SQL..";
+            statusLBL.Refresh();
+
+            int CharGuid = Convert.ToInt32(CharGuidTXT.Text);
+
+            SqlOutRTB.AppendText($"-- Character Spells {Environment.NewLine}");
+            SqlOutRTB.AppendText($"DELETE FROM `character_spell` WHERE `guid` = {CharGuid}; {Environment.NewLine}");
+            foreach (var spellID in SpellIDList)
+                SqlOutRTB.AppendText($"INSERT INTO `character_spell`(`guid`, `spell`, `active`, `disabled`) VALUES({CharGuid}, {spellID}, 1, 0); {Environment.NewLine}");
+
+            SqlOutRTB.AppendText($"-- Character Achievement {Environment.NewLine}");
+            SqlOutRTB.AppendText($"DELETE FROM `character_achievement` WHERE `guid` = {CharGuid}; {Environment.NewLine}");
+            for (int i = 0; i < AchiID.Count; i++)
+                SqlOutRTB.AppendText($"INSERT INTO `character_achievement`(`guid`, `achievement`, `date`) VALUES({CharGuid}, {AchiID[i]}, {DateAchi[i]}); {Environment.NewLine}");
+
+            SqlOutRTB.AppendText($"-- Character Achievement In Progress {Environment.NewLine}");
+            SqlOutRTB.AppendText($"DELETE FROM `character_achievement_progress` WHERE `guid` = {CharGuid}; {Environment.NewLine}");
+            for (int i = 0; i < AchiProgID.Count; i++)
+                SqlOutRTB.AppendText($"INSERT INTO `character_achievement_progress`(`guid`, `criteria`, `counter`, `date`) VALUES({CharGuid}, {AchiProgID[i]}, {QuantityAchiProg[i]}, {DateAchiProg[i]}); {Environment.NewLine}");
+
+            SqlOutRTB.AppendText($"-- Character Item Instance {Environment.NewLine}");
+            SqlOutRTB.AppendText($"DELETE FROM `item_instance` WHERE `owner_guid` = {CharGuid}; {Environment.NewLine}");
+            MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;Database=characters;Uid=root;Pwd=ascent;");
+            conn.Open();
+            for(int i = 0; i < ItemEntry.Count; i++)
+            {
+                MySqlCommand cmd = new MySqlCommand("SELECT max(`guid`) as 'maxguid' FROM `item_instance`;", conn);
+                MySqlDataReader r = cmd.ExecuteReader();
+                int maxguid = 0;
+                while (r.Read())
+                    maxguid = Convert.ToInt32(r["maxguid"]);
+                r.Close();
+                SqlOutRTB.AppendText($"INSERT INTO `item_instance`(`guid`, `owner_guid`, `itemEntry`, `count`, `flags`, `durability`) VALUES({maxguid}, {CharGuid}, {ItemEntry[i]}, {ItemCount[i]}, {ItemFlags[i]}, {ItemDurability[i]}); {Environment.NewLine}");
+            }
+            conn.Close();
+            statusLBL.ResetText();
+            statusLBL.Text = "NO STATUS";
+            statusLBL.Refresh();
+        }
+    }
+}
